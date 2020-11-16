4fd43afaa2476c760a1a2434de22b0389b8cfe97
mohuishou <1@lailin.xyz>
2020-11-17 00:15:17 +0800

    fix: 修复 matter yaml 存在 : 的bug

--- adapter/hexo.js
+++ adapter/hexo.js
@@ -1,18 +1,15 @@
-'use strict';
+"use strict";
 
-const ejs = require('ejs');
-const Entities = require('html-entities').AllHtmlEntities;
-const FrontMatter = require('hexo-front-matter');
-const { formatDate, formatRaw, formatTags, formatList } = require('../util');
+const ejs = require("ejs");
+const Entities = require("html-entities").AllHtmlEntities;
+const FrontMatter = require("hexo-front-matter");
+const { formatDate, formatRaw, formatTags, formatList } = require("../util");
 
 const entities = new Entities();
 
 // 文章模板
 const template = `---
-<% for (const key in props) {-%>
-<%= key %>: <%= props[key] %>
-<% } -%>
----
+<%- matter -%>
 
 <%- raw -%>`;
 
@@ -29,7 +26,7 @@ function parseMatter(body) {
   try {
     // front matter信息的<br/>换成 \n
     const regex = /(title:|layout:|tags:|date:|categories:){1}(\S|\s)+?---/gi;
-    body = body.replace(regex, a => a.replace(/(<br \/>|<br>|<br\/>)/gi, '\n'));
+    body = body.replace(regex, (a) => a.replace(/(<br \/>|<br>|<br\/>)/gi, "\n"));
     const result = FrontMatter.parse(body);
     result.body = result._content;
     if (result.date) {
@@ -50,7 +47,7 @@ function parseMatter(body) {
  * @param {Object} post 文章
  * @return {String} text
  */
-module.exports = function(post) {
+module.exports = function (post) {
   // matter 解析
   const parseRet = parseMatter(post.body);
   const { body, ...data } = parseRet;
@@ -60,16 +57,16 @@ module.exports = function(post) {
   const tags = data.tags || [];
   const categories = data.categories || [];
   const props = {
-    title: title.replace(/"/g, ''), // 临时去掉标题中的引号，至少保证文章页面是正常可访问的
+    title: title.replace(/"/g, ""), // 临时去掉标题中的引号，至少保证文章页面是正常可访问的
     urlname,
     date,
     ...data,
-    tags: formatTags(tags),
-    categories: formatList(categories),
+    tags: tags,
+    categories: categories,
   };
   const text = ejs.render(template, {
     raw,
-    props,
+    matter: FrontMatter.stringify(props),
   });
   return text;
 };
